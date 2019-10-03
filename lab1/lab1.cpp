ffbf782ba01a6532fa91e4c6a2fb24a891bc147e
Hasan Ali <hasanhaja@gmail.com>
2019-10-03 14:57:08 +0100

    Refactored solution generator functions to generic function

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,124 +1,91 @@
+#include <functional>
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
-double f(double x) {
-	return 3.0*(x*x*x) + 2.0*x + 1.0;
-}
+double f(double x) { return 3.0 * (x * x * x) + 2.0 * x + 1.0; }
 
-double fPrime(double x) {
-	return 9.0*(x*x) + 2.0;
-}
+double fPrime(double x) { return 9.0 * (x * x) + 2.0; }
 
 double forwardDifference(double x, double deltax) {
-	return (f(x + deltax) - f(x)) / deltax;
+    return (f(x + deltax) - f(x)) / deltax;
 }
 
 double backwardDifference(double x, double deltax) {
-	return (f(x) - f(x - deltax)) / deltax;
+    return (f(x) - f(x - deltax)) / deltax;
 }
 
 double centralDifference(double x, double deltax) {
-	return (f(x + deltax) - f(x - deltax)) / (2.0*deltax);
+    return (f(x + deltax) - f(x - deltax)) / (2.0 * deltax);
 }
 
 double measureAccuracy(double calculated, double analytical) {
-	return analytical - calculated;
+    return analytical - calculated;
 }
 
-// TODO try to add lambda to specify actual function.
-std::vector<double> analyticalSolution(double start, double end, double deltax) {
-	
-	std::vector<double>	results;	
+std::vector<double> analyticalSolution(double start, double end, double deltax,
+                                       std::function<double(double)> method) {
+    std::vector<double> results;
 
-	for (double x = start; x <= end; ) {
-		double result = fPrime(x);
-		results.push_back(result);
-		x += deltax;
-	} 
+    for (double x = start; x <= end;) {
+        double result = method(x);
+        results.push_back(result);
+        x += deltax;
+    }
 
-	return results;
+    return results;
 }
 
-std::vector<double> forwardSolution(double start, double end, double deltax) {
-	
-	std::vector<double>	results;	
+std::vector<double> finiteSolution(
+    double start, double end, double deltax,
+    std::function<double(double, double)> method) {
+    std::vector<double> results;
 
-	for (double x = start; x <= end; ) {
-		double result = forwardDifference(x, deltax);
-		results.push_back(result);
-		x += deltax;
-	} 
+    for (double x = start; x <= end;) {
+        double result = method(x, deltax);
+        results.push_back(result);
+        x += deltax;
+    }
 
-	return results;
-}
-
-std::vector<double> backwardSolution(double start, double end, double deltax) {
-	
-	std::vector<double>	results;	
-
-	for (double x = start; x <= end; ) {
-		double result = backwardDifference(x, deltax);
-		results.push_back(result);
-		x += deltax;
-	} 
-
-	return results;
-}
-
-std::vector<double> centralSolution(double start, double end, double deltax) {
-	
-	std::vector<double>	results;	
-
-	for (double x = start; x <= end; ) {
-		double result = centralDifference(x, deltax);
-		results.push_back(result);
-		x += deltax;
-	} 
-
-	return results;
+    return results;
 }
 
 void display(double in, double result) {
-	std::cout << "f(" << in << ") = " << result << std::endl;
+    std::cout << "f(" << in << ") = " << result << std::endl;
 }
 
-void show(std::string msg) {
-	std::cout << msg << std::endl;
-}
+void show(std::string msg) { std::cout << msg << std::endl; }
 
 int main() {
-	
+    double start = 0.0;
+    double end = 1.0;
+    double deltax = 0.1;
 
-	double start = 0.0;
-	double end = 1.0;
-	double deltax = 0.1;	
+    auto actualResults = analyticalSolution(start, end, deltax, fPrime);
+    auto forwardResults = finiteSolution(start, end, deltax, forwardDifference);
+    auto backwardResults =
+        finiteSolution(start, end, deltax, backwardDifference);
+    auto centralResults = finiteSolution(start, end, deltax, centralDifference);
 
-	std::vector<double> actualResults = analyticalSolution(start, end, deltax);
-	std::vector<double> forwardResults = forwardSolution(start, end, deltax);
-	std::vector<double> backwardResults = backwardSolution(start, end, deltax);
-	std::vector<double> centralResults = centralSolution(start, end, deltax);
- 
-	
-	show("Actual results:");
-	for (auto result : actualResults) {
-		std::cout << result << std::endl;
-	}
-	
-	show("Forward results:");
-	for (auto result : forwardResults) {
-		std::cout << result << std::endl;
-	}
+    show("Actual results:");
+    for (auto result : actualResults) {
+        std::cout << result << std::endl;
+    }
 
-	show("Backward results:");
-	for (auto result : backwardResults) {
-		std::cout << result << std::endl;
-	}
+    show("Forward results:");
+    for (auto result : forwardResults) {
+        std::cout << result << std::endl;
+    }
 
-	show("Central results:");
-	for (auto result : centralResults) {
-		std::cout << result << std::endl;
-	}
-	
-	return 0;
+    show("Backward results:");
+    for (auto result : backwardResults) {
+        std::cout << result << std::endl;
+    }
+
+    show("Central results:");
+    for (auto result : centralResults) {
+        std::cout << result << std::endl;
+    }
+
+    return 0;
 }
